d26c59e00b7e2ba1d9000d1055c8b2d7f8a3365f
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-08-22 23:07:22 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -230,8 +230,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 	title: `${htki} MAIN ${htka}`,
 	rows: [
 	    {title: `⚡ ${pmenus} SPEED BOT`, rowId: ".ping", description: "Menampilkan kecepatan respon BOT"},
-	    {title: `💌 ${pmenus} OWNER BOT`, rowId: ".owner", description: "Menampilkan List owner BOT"},
-	    {title: `📔 ${pmenus} SCRIPT BOT`, rowId: ".sc", description: `Source Code ${namebot}`},
+	    {title: `💌 ${pmenus} OWNER BOT`, rowId: ".owner", description: "Menampilkan List owner BOT"}, 
 	]
     },{
 	title: `${htki} SUPPORT ${htka}`,
